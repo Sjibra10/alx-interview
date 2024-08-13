5c24e780c525bc91ffe1f077fcd4bffc28d67913
Sjibra10 <buhlebuka@gmail.com>
2024-08-13 14:54:58 +0000

    using sets for unique sets

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""interview question lockbozxes"""
+
+
+def canUnlockAll(boxes):
+    """unloack function"""
+    n = len(boxes)
+    visited = set()
+    stack = [0]
+
+    while stack:
+        box = stack.pop()
+        visited.add(box)
+
+        for key in boxes[box]:
+            if key < n and key not in visited:
+                stack.append(key)
+
+    return len(visited) == n
