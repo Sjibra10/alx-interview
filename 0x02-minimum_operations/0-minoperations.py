bb5df92c5de0a82436bb82589399cc49d63487d3
Sjibra10 <buhlebuka@gmail.com>
2024-08-21 17:52:29 +0000

    using two pointers

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,24 +1,21 @@
 #!/usr/bin/python3
-"""main code base"""
+"""Module"""
 
 
 def minOperations(n):
-    """
-    Calculates the minimum number of operations to get n 'H' characters.
-    Args:
-    n: The target number of 'H' characters.
-    Returns:
-    The minimum number of operations needed, or 0 if impossible.
-    """
-
+    """ calculates the fewest number of operations needed
+        to result in exactly n H
+        characters in the file."""
     if n <= 1:
         return 0
 
-    dp = [float('inf')] * (n + 1)
-    dp[1] = 0
-
-    for i in range(2, n + 1):
-        for j in range(1, i):
-            if i % j == 0:
-                dp[i] = min(dp[i], dp[j] + i // j)
-    return dp[n] if dp[n] != float('inf') else 0
+    operations = 0
+    current_h = 1
+    clipboard = current_h
+    while current_h < n:
+        if n % current_h == 0:
+            clipboard = current_h
+            operations += 1
+        current_h += clipboard
+        operations += 1
+    return operations
