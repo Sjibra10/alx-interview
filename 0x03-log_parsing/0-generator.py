0918ad17a7e0bea6f3921336dbbba211f2a7a930
Sjibra10 <buhlebuka@gmail.com>
2024-08-27 16:41:54 +0000

    generate

--- 0x03-log_parsing/0-generator.py
+++ 0x03-log_parsing/0-generator.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
+"""generator file"""
+
+
 import random
 import sys
 from time import sleep
 import datetime
+
 
 for i in range(10000):
     sleep(random.random())
@@ -12,3 +16,4 @@ for i in range(10000):
         random.choice([200, 301, 400, 401, 403, 404, 405, 500]),
         random.randint(1, 1024)
     ))
+    sys.stdout.flush()
